df29ed46d1ea7aa43897e45aa9db3c4308ea91c6
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-10-02 13:58:06 +0200

    Gardening

--- lib/sandbox.rb
+++ lib/sandbox.rb
@@ -1,34 +1,30 @@
 $:.unshift(File.dirname(__FILE__)) unless \
     $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-
 module Sandbox
-  
   class << self
-    
+
     def verbosity
       @verbosity ||= 0
     end
-    
+
     def increase_verbosity
       @verbosity = verbosity + 1
     end
-    
+
     def decrease_verbosity
       @verbosity = verbosity - 1
     end
-    
+
     def quiet?() verbosity < 0 end
     def really_quiet?() verbosity < -1 end
     def verbose?() verbosity > 0 end
     def really_verbose?() verbosity > 1 end
-    
+
   end
-  
 end
 
 require 'sandbox/version'
 require 'sandbox/errors'
 require 'sandbox/output'
 require 'sandbox/installer'
-
