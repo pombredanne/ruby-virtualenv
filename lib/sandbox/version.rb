51ebb51e11ed9c5ead19533384e07730e3b9d50d
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-10-02 13:57:48 +0200

    Gardening

--- lib/sandbox/version.rb
+++ lib/sandbox/version.rb
@@ -1,15 +1,11 @@
-
 module Sandbox
-  
   class Version #:nodoc:
-    
+
     MAJOR = 0
     MINOR = 2
     TINY  = 4
 
     STRING = [ MAJOR, MINOR, TINY ].join( '.' )
-    
-  end
-  
-end
 
+  end
+end
