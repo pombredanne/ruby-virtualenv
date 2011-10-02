51a7ded373a478382c83f4b8a388d000b7e84a5a
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-10-02 14:02:50 +0200

    Gardening

--- spec/sandbox_spec.rb
+++ spec/sandbox_spec.rb
@@ -1,25 +1,25 @@
-
 require File.dirname( __FILE__ ) + '/spec_helper.rb'
 
 describe Sandbox do
+
   before( :each ) do
-    Sandbox.instance_eval { instance_variables.each { |v| remove_instance_variable v } }
+    Sandbox.instance_eval { instance_variables.each { |v| remove_instance_variable(v) } }
   end
-  
+
   it "should have verbosity level" do
     Sandbox.verbosity.should == 0
   end
-  
+
   it "should increase verbosity level" do
     Sandbox.verbosity.should == 0
     Sandbox.increase_verbosity
     Sandbox.verbosity.should == 1
   end
-  
+
   it "should decrease verbosity level" do
     Sandbox.verbosity.should == 0
     Sandbox.decrease_verbosity
     Sandbox.verbosity.should == -1
   end
-end
 
+end
